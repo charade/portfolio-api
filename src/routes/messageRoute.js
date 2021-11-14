4aec799f3ee62fdbe6084e2f1c771a8d10068d46
Charles.Ek <macbook@MacBook.local>
2021-11-14 14:37:37 +0100

    :sparkles:email validation

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -1,37 +1,37 @@
 require('dotenv').config();
 const router = require('express').Router();
 const emailCheck = require('email-validator');
-const { response } = require('express');
 const nodemailer = require('nodemailer');
 
 router.post('/',(req, res) => {
     const { message, email } = req.body;
+
     isValidEmail  = emailCheck.validate(email);
 
-    const transport  = nodemailer.createTransport({
-        service : 'gmail',
-        auth : {
-            user: process.env.EMAIL,
-            pass : process.env.EMAIL_PASSWORD
-        }
-    })
-    const emailOption = {
-        from : email,
-        to : process.env.EMAIL,
-        subject : "I saw your portfolio",
-        html : `<h2>${email}</h2><p>${message}</p>`
-    }
-
-    transport.sendMail(emailOption, (error, response) => {
-        if(error){
-            console.log(error)
-           
-            res.status(400)
-            return;
-        }
-        res.status(200)
-    })
-
+    if(isValidEmail){
+        const transport  = nodemailer.createTransport({
+            service : 'gmail',
+            auth : {
+                user: process.env.EMAIL,
+                pass : process.env.EMAIL_PASSWORD
+            }
+        });
+        const emailOption = {
+            from : email,
+            to : process.env.EMAIL,
+            subject : "I saw your portfolio",
+            html : `<h2>${email}</h2><p>${message}</p>`
+        };
+        transport.sendMail(emailOption, (error, response) => {
+            if(error){
+                res.status(400)
+                return;
+            }
+            res.status(200).json(response)
+        });
+        return;
+    };
+    res.status(400).json({error : "bad email"})
 });
 
 module.exports = router;
